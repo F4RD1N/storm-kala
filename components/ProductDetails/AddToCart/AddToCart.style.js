7f258eba59b4acfb985dc5d3d8b97eca45a0638d
F4RD1N <fardin5727@gmail.com>
2022-07-24 02:52:07 +0430

    change add to cart position to sticky

--- components/ProductDetails/AddToCart/AddToCart.style.js
+++ components/ProductDetails/AddToCart/AddToCart.style.js
@@ -1,26 +1,26 @@
 import styled from "styled-components";
 
 //Shared Styled
-import {SectionCard, GlobalButton} from '../../../styles/shared'
+import { SectionCard, GlobalButton } from "../../../styles/shared";
 export const Container = styled(SectionCard)`
-    position: fixed;
-    bottom: 0;
-    width: 100%;
-    right: 0;
-    left: 0;
-    z-index: 5;
-    box-shadow: 1px 1px 15px rgba(0, 0, 0, 0.1);
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-`
+  position: sticky;
+  bottom: 0;
+
+  z-index: 5;
+  box-shadow: 1px 1px 15px rgba(0, 0, 0, 0.05);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  margin-bottom: 3rem;
+`;
 
 export const AddButton = styled(GlobalButton)`
-    font-size: 1rem;
-    padding: .8rem 2rem;
-`
+  font-size: 1rem;
+  padding: 0.8rem 2rem;
+`;
 
 export const ProductPrice = styled.span`
-    font-size: 1rem;
-    font-weight: bold;
-`
+  font-size: 1rem;
+  font-weight: bold;
+`;
