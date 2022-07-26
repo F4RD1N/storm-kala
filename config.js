6c77da09bb0130dc26f599632ee082c3535ceffb
F4RD1N <fardin5727@gmail.com>
2022-07-26 18:07:21 +0430

    change api endpoint

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 //set url to axios
 import axios from 'axios';
-axios.defaults.baseURL = 'http://localhost:3000/api';
+axios.defaults.baseURL = '/api';
--- helpers/pusher.js
+++ helpers/pusher.js
@@ -1,7 +1,7 @@
 export const dataPusher = (url, dispatcher) => {
   return {
     endpoint: (page) =>
-      `http://localhost:3000/api/${url}page=${page}`,
+      `/api/${url}page=${page}`,
     action: (payload) => dispatcher(payload),
     dataPath: (path) => path.data?.data.products,
   };
