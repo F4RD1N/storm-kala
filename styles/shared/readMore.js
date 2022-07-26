8159e8b0c3213ffeb82b0df305bfc31a0f554631
F4RD1N <fardin5727@gmail.com>
2022-07-26 14:36:19 +0430

    undo changes

--- styles/shared/readMore.js
+++ styles/shared/readMore.js
@@ -2,9 +2,8 @@ import styled from "styled-components";
 
 const ReadMore = styled.span`
   position: absolute;
-  bottom: 0;
+  bottom: 10px;
   padding-top: ${props => props.showFull ? '' : '5rem'};
-  padding-bottom: 1rem;
   color: ${(props) => props.theme.colors.blue};
   margin: 0 0.5rem;
   margin-top: 1rem;
