a221a1e1a6c2754b6e892462bf8f14b1ce2fa912
F4RD1N <fardin5727@gmail.com>
2022-07-27 13:51:20 +0430

    filter home data to prevent render duplicate items

--- components/useHomeState.js
+++ components/useHomeState.js
@@ -3,14 +3,15 @@ import { useSelector } from "react-redux";
 const useHomeState = () => {
   const state = useSelector((state) => state.homeState);
 
+  // array of objects are filtered by id because there are duplicate items in the api
   return {
-    incredibleProducts: state.incredibleProducts,
-    popularBrands: state.popularBrands,
-    bestSellingProducts: state.bestSellingProducts,
-    categories: state.categories,
-    recommendationSubCategories: state.recommendationSubCategories,
-    mobileList: state.mobileList,
-    laptopList: state.laptopList
+    incredibleProducts: state.incredibleProducts?.filter((v,i,a)=>a.findIndex(v2=>(v2.id===v.id))===i),
+    popularBrands: state.popularBrands?.filter((v,i,a)=>a.findIndex(v2=>(v2.id===v.id))===i),
+    bestSellingProducts: state.bestSellingProducts?.filter((v,i,a)=>a.findIndex(v2=>(v2.id===v.id))===i),
+    categories: state.categories?.filter((v,i,a)=>a.findIndex(v2=>(v2.id===v.id))===i),
+    recommendationSubCategories: state.recommendationSubCategories?.filter((v,i,a)=>a.findIndex(v2=>(v2.id===v.id))===i),
+    mobileList: state.mobileList?.filter((v,i,a)=>a.findIndex(v2=>(v2.id===v.id))===i),
+    laptopList: state.laptopList?.filter((v,i,a)=>a.findIndex(v2=>(v2.id===v.id))===i),
   };
 };
 
