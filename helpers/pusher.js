aeeab618f09e8c59c29dec35fdbef551335407e0
F4RD1N <fardin5727@gmail.com>
2022-08-20 18:05:36 +0430

    delete pusher.js file

--- helpers/pusher.js
+++
@@ -1,9 +0,0 @@
-export const dataPusher = (url, type) => {
-  return {
-    endpoint: (page) => `${url}page=${page}`,
-    type,
-  };
-};
-
-//url => the endpoint of api need to fetch
-// type => the name of the property of the state to push data to it
